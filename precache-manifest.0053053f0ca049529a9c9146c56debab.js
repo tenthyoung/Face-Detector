bfe911989bfcf3c58161fdccce31d9aeaedc6dfe
TenthYoung <tenthyoung@gmail.com>
2019-08-13 14:47:27 -0700

    Updates

---
+++ precache-manifest.0053053f0ca049529a9c9146c56debab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8662cfbd17c54ad80954",
+    "url": "/tenthyoung/Face-Detector/static/js/runtime~main.8662cfbd.js"
+  },
+  {
+    "revision": "c6ef705cc79489d8ce7f",
+    "url": "/tenthyoung/Face-Detector/static/js/main.c6ef705c.chunk.js"
+  },
+  {
+    "revision": "49c830226a87f7e0b848",
+    "url": "/tenthyoung/Face-Detector/static/js/2.49c83022.chunk.js"
+  },
+  {
+    "revision": "c6ef705cc79489d8ce7f",
+    "url": "/tenthyoung/Face-Detector/static/css/main.c4da1c77.chunk.css"
+  },
+  {
+    "revision": "49c830226a87f7e0b848",
+    "url": "/tenthyoung/Face-Detector/static/css/2.810008eb.chunk.css"
+  },
+  {
+    "revision": "5555ce1cfdbc5cb25c8aba3af11e96b2",
+    "url": "/tenthyoung/Face-Detector/index.html"
+  }
+];
